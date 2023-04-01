a6148198ae6f4b82ec42b5936a014f096b319b0c
fellipe1908 <fellipe1908@alu.ufc.br>
2023-04-01 12:09:50 -0300

    Exercicio Resolvido

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ metros_quadrados = float(metros_quadrados)
 
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
-qtd_de_latas = 0
+qtd_de_latas = metros_quadrados // 54
+qtd_de_latas2 = metros_quadrados / 54
 valor_total = 0
-
-print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
+if qtd_de_latas2 > qtd_de_latas :
+    lt = qtd_de_latas + 1
+    v = lt * 80
+    print(f"Serão necessárias {lt} latas totalizando R$ {v}")
+else:
+    qtd_de_latas
+    valor = qtd_de_latas * 80
+    print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor}")
